e3615d78c2ff8745d1ac16543a4b09801d97f987
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-27 14:45:46 -0500

    Ejecutable

---
+++ app.py
@@ -0,0 +1,9 @@
+from services import * 
+# Importa todas las funciones y variables del modulo services
+
+if __name__ == "__main__":
+    print("")
+    print("=" * 50)
+    print("   WELCOME TO THE SYSTEM")
+    print("=" * 50)
+# Esto hace que todo se ejecute en app.py (es el archivo principal)
